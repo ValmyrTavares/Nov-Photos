4c84ba8e6cc849e80ef684ae67e817da630630dc
Valmyr Tavares <valmyr.lima@millennium.com.br>
2019-11-07 18:09:13 -0300

    Meu primeiro comit

---
+++ app/js/models/Negociacao.js
@@ -0,0 +1,16 @@
+class Negociacao {
+    constructor(data, quantidade, valor) {
+    }
+    get data() {
+        return this._data;
+    }
+    get quantidade() {
+        return this._quantidade;
+    }
+    get valor() {
+        return this._valor;
+    }
+    get volume() {
+        return this._quantidade * this._valor;
+    }
+}
